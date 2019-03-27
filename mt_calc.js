b080f523f6cb80c0ee95d66903036a6508799866
Unknown <TAdcox009@west-mec.org>
2019-03-26 18:20:32 -0700

    final commit
    Added comments,  added the window onload command, debugged code, added variables and new functions.

--- mt_calc.js
+++ mt_calc.js
@@ -33,53 +33,76 @@
 
 */
 
-// 4.Init() function sets up the event handlers for the page.
-// a. Declare the calcbuttons variable containing the collection of page elements belonging to the calcButton class.
-// b. Loop through the calcButtons object collection and, for each button in that collection, run the buttonClick() function in response to the click event.
-// c. After the for loop, add a command that runs the calcKeys() function in response to the key- down event occurring within the element with the ID “calcWindow”.
+// Loads the init function when the window is opened.
+window.onload = init;
+
+// Init function that has no paramaters. 
+// calcButtons variable that gets the element by its class name that affects all the input boxes with that class.
+// A for loop that loops through the calcButtons variable when it is clicked.
+// When the keydown event happens it will also run the calcKeys function.
 function init() {
       var calcButtons = document.getElementsByClassName("calcButton");
 
-      for (var i = 0; i < calcButtons; i++) {
-            document.addEventListener("click", buttonClick);
+      for (var i = 0; i < calcButtons.length; i++) {
+            calcButtons[i].onclick = buttonClick;
       }
-      document.getElementById(calcWindow).addEventListener("keydown", calcKeys);
+      document.getElementById("calcWindow").onkeydown = calcKeys;
 }
 
-// 5. Create the buttonClick() function. The purpose of this function is to change what appears in the calculator window when the user clicks the calculator buttons. Add the following commands to the function: 
-// a. Declare the calcvalue variable equal to the value attribute of the calcWindow text area box. 
-// b. Declare the calcDecimal variable equal to the value attribute of the decimals input box.
-// c. Each calculator button has a value attribute that defines what should be done with that button. Declare the buttonvalue attribute equal to the value attribute of the event object target.
-// d. Create a switch-case structure for the following possible values of the buttonValue variable:
-// Create a switch-case structure for the following possible values of the buttonValue variable: 
-// i.	For “del”, delete the contents of the calculate window by changing calcValue to an empty
-// text string.
-// ii.	For “bksp”, erase the last character in the calculator window by changing calcValue to the value returned by the eraseChar() function using calcValue as the parameter value.
-// iii.	For “enter”, calculate the value of the current expression by changing calcValue to:
-// “ = “ + evalEq(calcValue, calcDecimal) + “\n” Note that \n is used to add a line return at the end of the answer.
-// iv.	For “prev”, copy the last equation in the calculator window by changing calcValue to the value returned by the lastEq() function using calcValue as the parameter value.
-// v.	Otherwise, append the calculator button character to the calculator window by letting, calcValue equal calcValue plus buttonValue.
+// The buttonClick() function that changes what appears in the calculator window when the user clicks a button.
+// calcvalue variable thats equal to the value attribute of the calcWindow text area box. 
+// calcDecimal variable thats equal to the value attribute of the decimals input box.
+// buttonvalue variable that equals to the value attribute of the event object target.
+// For the “del” case that deletes the contents of the calculate window.
+// For the “bksp” case it  deletes the last character in the calculator window.
+// For the “enter” case it calculate the value of the current expression.
+// For the “prev” case it copies the last equation in the calculator window.
 function buttonClick(e) {
-      var calcValue = document.getElementById("calcWindow");
+      var calcValue = document.getElementById("calcWindow").value;
       var calcDecimal = document.getElementById("decimals").value;
       var buttonValue = e.target.value;
       switch (buttonValue) {
             case "del":
-                  calcValue += "";
+                  calcValue = "";
                   break;
             case "bksp":
-                  calcValue += eraseChar(calcValue)
+                  calcValue = eraseChar(calcValue)
                   break;
             case "enter":
-                  "=" + evalEq(calcValue);
+                  calcValue += " = " + evalEq(calcValue, calcDecimal) + "\n";
                   break;
             case "prev":
-                  calcValue += lastEq(calcValue);
+                  calcValue += lastEq(calcValue) + "\n";
                   break;
-            case "":
-                  calcValue += calcValue + buttonValue;
+            default:
+                  calcValue += buttonValue;
                   break;
       }
+      document.getElementById("calcWindow").value = calcValue;
+      document.getElementById("calcWidnow").focus();
+}
+
+// The calcKeys() function that contains the calcValue and calcDecimal variables. 
+// For the “Delete” case it erases the contents of the calculator window by changing it to an empty text string.
+// For the “Enter” case it adds the expression to the calcValue varible.
+// For the “ArrowUp” case adds the expression to calcValue variable.
+// The value attribute of the calcWindow text area box is set equal to the calcValue variable.
+
+function calcKeys(e) {
+      var calcValue = document.getElementById("calcWindow").value;
+      var calcDecimal = document.getElementById("decimals").value;
+      switch (e.key) {
+            case "Delete":
+                  calcValue = "";
+                  break;
+            case "Enter":
+                  calcValue += " = " + evalEq(calcValue, calcDecimal);
+                  break;
+            case "ArrowUp":
+                  calcValue += lastEq(calcWindow.value);
+                  e.preventDefault();
+      }
+      document.getElementById("calcWindow").value = calcValue;
 }
 
 /* ===================================================================== */
